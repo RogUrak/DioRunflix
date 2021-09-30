5576e4494bd977879d842800e156211cb16f8b08
RogUrak <rogerio.urakawa@gmail.com>
2021-09-30 17:47:17 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,18 @@
+## DESAFIO DIO - Runflix
+
+Objetivo: Recriar a interface do Netflix, baseado nos conceitos básicos de front-end.
+
+Projeto: Criação do RUNFLIX, site que mostra e direciona para os websites das principais maratonas do mundo (Six Majors). 
+
+#### Ferramentas utilizadas: 
+
+- HTML5
+- CSS3
+- JavaScript
+- Visual Studio Code
+  - Extensões: Live Server, Auto Rename Tag
+
+#### Recursos adicionais: 
+
+- Font Awesome (ícones para botões)
+- Owl Carousel (plugin para responsividade da página html)
---
+++ js/owl/setup.js
@@ -0,0 +1,44 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function botaoAbbott(){
+    window.open("https://www.worldmarathonmajors.com/");
+}
+
+function tokyo(){
+    window.open("https://www.marathon.tokyo/en/");
+}
+
+function boston(){
+    window.open("https://www.baa.org/");
+}
+
+function london(){
+    window.open("https://www.virginmoneylondonmarathon.com/");
+}
+
+function berlin(){
+    window.open("https://www.bmw-berlin-marathon.com/en/");
+}
+
+function chicago(){
+    window.open("https://www.chicagomarathon.com/");
+}
+
+function newyork(){
+    window.open("https://www.nyrr.org/tcsnycmarathon");
+}
